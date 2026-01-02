24e56318204a493a8c207e1c63215b66a1d7582f
Jonathan Peh <jonathan.peh.qj@gmail.com>
2026-01-02 12:19:18 +0800

    Update 1. Two Sum.py

--- code/1. Two Sum.py
+++ code/1. Two Sum.py
@@ -1,1 +1,18 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        index = {}
+        for i in range(len(nums)):
+            num = nums[i]
+            if num in index:
+                index[num].append(i)
+            else:
+                index[num] = [i]
 
+        for i in range(len(nums)):
+            num = nums[i]
+            complement = target - num
+            if complement in index:
+                js = index[complement]
+                for j in js:
+                    if j != i:
+                        return [j,i]
